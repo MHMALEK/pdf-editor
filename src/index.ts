872d9c82610b3f293788d80ed46bedd8fe6f0278
M.H.Malek <mhos.malek@gmail.com>
2023-07-09 15:12:56 +0200

    first version

--- src/index.ts
+++ src/index.ts
@@ -1,275 +1,296 @@
 import { fabric } from "fabric";
 import * as pdfjsLib from "pdfjs-dist";
-// One of the following themes
-import "@simonwep/pickr/dist/themes/classic.min.css"; // 'classic' theme
-import "@simonwep/pickr/dist/themes/monolith.min.css"; // 'monolith' theme
-import "@simonwep/pickr/dist/themes/nano.min.css"; // 'nano' theme
-
-// Modern or es5 bundle (pay attention to the note below!)
-import Pickr from "@simonwep/pickr";
-// Simple example, see optional options for more configuration.
-const pickr = Pickr.create({
-  el: ".color-picker",
-  theme: "classic", // or 'monolith', or 'nano'
-
-  swatches: [
-    "rgba(244, 67, 54, 1)",
-    "rgba(233, 30, 99, 0.95)",
-    "rgba(156, 39, 176, 0.9)",
-    "rgba(103, 58, 183, 0.85)",
-    "rgba(63, 81, 181, 0.8)",
-    "rgba(33, 150, 243, 0.75)",
-    "rgba(3, 169, 244, 0.7)",
-    "rgba(0, 188, 212, 0.7)",
-    "rgba(0, 150, 136, 0.75)",
-    "rgba(76, 175, 80, 0.8)",
-    "rgba(139, 195, 74, 0.85)",
-    "rgba(205, 220, 57, 0.9)",
-    "rgba(255, 235, 59, 0.95)",
-    "rgba(255, 193, 7, 1)",
-  ],
-
-  components: {
-    // Main components
-    preview: true,
-    opacity: true,
-    hue: true,
-
-    // Input / output Options
-    interaction: {
-      hex: true,
-      rgba: true,
-      hsla: true,
-      hsva: true,
-      cmyk: true,
-      input: true,
-      clear: true,
-      save: true,
-    },
-  },
-});
-
-if (!pdfjsLib.GlobalWorkerOptions.workerSrc) {
-  const WORKER_URL = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
-  pdfjsLib.GlobalWorkerOptions.workerSrc = WORKER_URL;
-}
 import { jsPDF } from "jspdf";
-function toggleEditor(show: boolean) {
-  var textControls = document.getElementById("wysiwyg-editor-container");
+import "@simonwep/pickr/dist/themes/classic.min.css";
+import Pickr from "@simonwep/pickr";
 
-  textControls.style.display = show ? "block" : "none";
+// Global variables
+let fabricCanvas: any;
+let isEditing = false;
+let color = "black";
+
+// Create a color picker
+const colorPicker = createColorPicker(".color-picker");
+
+// Ensure pdfjs worker is initialized
+initializePDFjsWorker();
+// setupEventListeners()
+document
+  .getElementById("pdf-upload-dropzone-file")
+  .addEventListener("change", (e) => {
+    handlePDFUpload(e);
+    toggleNavBar(true);
+  });
+
+document.getElementById("new-pdf-upload").addEventListener("click", () => {
+  toggleUploader(true);
+  removeCurrentPdf();
+});
+
+// Helper functions
+
+function handlePDFUpload(e: Event) {
+  const file = (e.target as HTMLInputElement).files[0];
+  const reader = new FileReader();
+
+  reader.onload = async (event) => {
+    const typedarray = new Uint8Array(event.target.result as ArrayBuffer);
+    // Load and render the first page of a PDF file
+    renderPDF(typedarray as ArrayBuffer, "pdf-canvas")
+      .then((canvas) => createFabricCanvas(canvas, "pdf-canvas"))
+      .then(() => setupEventListeners())
+      .then(() => toggleUploader(false));
+  };
+
+  reader.readAsArrayBuffer(file);
 }
 
-var url =
-  "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
+function createColorPicker(selector: string) {
+  return Pickr.create({
+    el: selector,
+    theme: "classic",
+    swatches: [
+      "rgba(244, 67, 54, 1)",
+      "rgba(233, 30, 99, 0.95)",
+      "rgba(156, 39, 176, 0.9)",
+      "rgba(103, 58, 183, 0.85)",
+      "rgba(63, 81, 181, 0.8)",
+      "rgba(33, 150, 243, 0.75)",
+      "rgba(3, 169, 244, 0.7)",
+      "rgba(0, 188, 212, 0.7)",
+      "rgba(0, 150, 136, 0.75)",
+      "rgba(76, 175, 80, 0.8)",
+      "rgba(139, 195, 74, 0.85)",
+      "rgba(205, 220, 57, 0.9)",
+      "rgba(255, 235, 59, 0.95)",
+      "rgba(255, 193, 7, 1)",
+    ],
 
-// // create a textbox and add it to the canvas
-// const textbox = new fabric.Textbox("Hello, world!", { left: 10, top: 10 });
-// canvass.add(textbox);
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
 
-pdfjsLib.getDocument(url).promise.then(function (pdf) {
-  // Load the first page
-  pdf.getPage(1).then(function (page) {
-    var viewport = page.getViewport({ scale: 1.0 });
-    var canvas: any = document.getElementById("pdf-canvas");
-    var container = document.getElementById("container");
-    var context = canvas.getContext("2d");
-    canvas.height = viewport.height;
-    canvas.width = viewport.width;
-
-    container.style.height = `${viewport.height}px`;
-    container.style.width = `${viewport.width}px`;
-    container.style.position = "absolute";
-    container.style.overflow = "hidden";
-
-    // Render the page on the canvas
-    var renderTask = page.render({
-      canvasContext: context,
-      viewport: viewport,
-    });
-
-    renderTask.promise.then(function () {
-      // Save the canvas as an image
-      var canvasImg = canvas.toDataURL();
-
-      let isEditing = false;
-
-      // Pass the canvas to Fabric.js
-      var fabricCanvas = new fabric.Canvas("pdf-canvas", {
-        isDrawingMode: false,
-      });
-
-      fabricCanvas.on("text:editing:entered", function (options) {
-        isEditing = true; // Set the flag to true when editing starts
-
-        toggleEditor(true);
-      });
-
-      fabricCanvas.on("text:editing:exited", function () {
-        isEditing = false; // Set the flag to false when editing ends
-        toggleEditor(false);
-      });
-
-      // Load the image back into the fabric canvas
-      fabric.Image.fromURL(canvasImg, function (img) {
-        img.selectable = false;
-        // Add the image back to the canvas
-        fabricCanvas.add(img);
-      });
-
-      // when the delete key is pressed, remove the currently active object
-      window.addEventListener("keydown", function (e) {
-        // key code 46 is the delete key
-        // key code 8 is the backspace key
-        if ((e.keyCode === 46 || e.keyCode === 8) && !isEditing) {
-          const activeObject = fabricCanvas.getActiveObject();
-
-          if (activeObject) {
-            fabricCanvas.remove(activeObject);
-          }
-        }
-      });
-
-      // Add an event handler for the button click
-      document
-        .getElementById("save-pdf")
-        .addEventListener("click", function () {
-          // Create a new jsPDF instance
-          var doc = new jsPDF();
-
-          // Get the content of the canvas as an image
-          var imgData = fabricCanvas.toDataURL({ format: "png" });
-
-          // Add the image to the PDF
-          doc.addImage(imgData, 0, 0, 0, 0);
-
-          // Save the PDF
-          doc.save("download.pdf");
-        });
-
-      document
-        .getElementById("add-text")
-        .addEventListener("click", function () {
-          // Add a text object
-          var text = new fabric.IText("Hello, world!", {
-            left: 100,
-            top: 100,
-          });
-          fabricCanvas.add(text);
-        });
-
-      document
-        .getElementById("font-size")
-        .addEventListener("change", function () {
-          console.log("asdasd");
-          updateStyles();
-        });
-
-      document
-        .getElementById("font-family")
-        .addEventListener("change", function () {
-          updateStyles();
-        });
-
-      fabricCanvas.on("selection:created", function (options) {
-        const activeObject = fabricCanvas.getActiveObject();
-        console.log(activeObject, options);
-        var textControls = document.getElementById("wysiwyg-editor-container");
-
-       
-
-        toggleEditor(true);
-        updateStyles();
-      });
-
-      fabricCanvas.on("selection:cleared", function () {
-        console.log("selection:cleared");
-        toggleEditor(false);
-      });
-
-      let color: string = "black";
-      const updateStyles = (items?: {
-        bold?: boolean;
-        italic?: boolean;
-        underline?: boolean;
-      }) => {
-        var activeObject = fabricCanvas.getActiveObject();
-
-        console.log(items?.underline);
-        // If there is a selected object and it's a text object, update its styles
-        if (activeObject) {
-          activeObject.set({
-            // @ts-ignore
-            fontSize: parseInt(document.getElementById("font-size").value),
-            fill: color,
-            // @ts-ignore
-            fontFamily: document.getElementById("font-family").value,
-            fontWeight: items?.bold ? "bold" : "normal",
-            fontStyle: items?.italic ? "italic" : "normal",
-            textDecoration: items?.underline ? "underline" : "",
-          });
-
-          fabricCanvas.renderAll();
-        }
-      };
-      document
-        .getElementById("boldButton")
-        .addEventListener("click", function () {
-          updateStyles({
-            bold: true,
-          });
-        });
-
-      document
-        .getElementById("italicButton")
-        .addEventListener("click", function () {
-          updateStyles({
-            italic: true,
-          });
-        });
-
-      document
-        .getElementById("underlineButton")
-        .addEventListener("click", function () {
-          updateStyles({
-            underline: true,
-          });
-        });
-
-      pickr.on("change", (e: any) => {
-        console.log(e);
-        color = e.toRGBA().toString();
-        updateStyles();
-      });
-
-      document
-        .getElementById("loadImage")
-        .addEventListener("click", function () {
-          var imageUrl = document.getElementById("imageUrl").value;
-
-          fabric.Image.fromURL(imageUrl, function (img) {
-            // Scale the image to 50% of its original size
-            var oImg = img.set({ left: 0, top: 0, angle: 0 }).scale(0.5);
-            fabricCanvas.add(oImg).renderAll();
-          });
-        });
-
-      document
-        .getElementById("imageUpload")
-        .addEventListener("change", function (e: any) {
-          var file = e.target.files[0];
-          var reader = new FileReader();
-
-          reader.onload = function (f) {
-            var data: any = f.target.result;
-            fabric.Image.fromURL(data, function (img: any) {
-              var oImg = img.set({ left: 0, top: 0 }).scale(1);
-              fabricCanvas.add(oImg).renderAll();
-              var a = fabricCanvas.setActiveObject(oImg);
-            });
-          };
-          reader.readAsDataURL(file);
-        });
-    });
+      // Input / output Options
+      interaction: {
+        hex: true,
+        rgba: true,
+        hsla: true,
+        hsva: true,
+        cmyk: true,
+        input: true,
+        clear: true,
+        save: true,
+      },
+    },
   });
-});
+}
+
+function initializePDFjsWorker() {
+  if (!pdfjsLib.GlobalWorkerOptions.workerSrc) {
+    const WORKER_URL = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+    pdfjsLib.GlobalWorkerOptions.workerSrc = WORKER_URL;
+  }
+}
+
+async function renderPDF(url: ArrayBuffer, canvasId: string) {
+  const pdf = await pdfjsLib.getDocument({ data: url }).promise;
+  const page = await pdf.getPage(1);
+  const viewport = page.getViewport({ scale: 1.0 });
+  const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+  const context = canvas.getContext("2d");
+
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  await page.render({ canvasContext: context, viewport }).promise;
+
+  return canvas;
+}
+
+function createFabricCanvas(canvas: HTMLCanvasElement, fabricCanvasId: string) {
+  const canvasImg = canvas.toDataURL();
+
+  fabricCanvas = new fabric.Canvas(fabricCanvasId, {
+    isDrawingMode: false,
+  });
+
+  fabricCanvas.on("text:editing:entered", () => {
+    isEditing = true;
+    setTimeout(() => {
+      toggleEditor(true);
+    }, 400);
+  });
+
+  fabricCanvas.on("text:editing:exited", () => {
+    isEditing = false;
+    resetStyles()
+    setTimeout(() => {
+      toggleEditor(false);
+    }, 400);
+  });
+
+  fabric.Image.fromURL(canvasImg, (img) => {
+    img.selectable = false;
+    fabricCanvas.add(img);
+  });
+
+  fabricCanvas.on("selection:created");
+  fabricCanvas.on("selection:cleared", () => {
+    toggleEditor(false);
+  });
+}
+
+function setupEventListeners() {
+  // Set up event listeners for your buttons, input elements, etc.
+
+  document.getElementById("save-pdf").addEventListener("click", savePDF);
+  document.getElementById("add-text").addEventListener("click", addText);
+  document.getElementById("font-size").addEventListener("change", updateStyles);
+  document
+    .getElementById("font-family")
+    .addEventListener("change", updateStyles);
+  document
+    .getElementById("boldButton")
+    .addEventListener("click", () => updateStyles({ bold: true }));
+  document
+    .getElementById("italicButton")
+    .addEventListener("click", () => updateStyles({ italic: true }));
+
+  colorPicker.on("change", (e: any) => {
+    color = e.toRGBA().toString();
+    updateStyles();
+  });
+
+  window.addEventListener("keydown", deleteActiveObject);
+  document.getElementById("loadImage").addEventListener("click", loadImage);
+  document
+    .getElementById("imageUpload")
+    .addEventListener("change", handleImageUpload);
+
+  document.getElementById("h1FormatButton").addEventListener("click", () => {
+    formatBlock("H1");
+  });
+  document.getElementById("h2FormatButton").addEventListener("click", () => {
+    formatBlock("H2");
+  });
+  document.getElementById("h3FormatButton").addEventListener("click", () => {
+    formatBlock("H3");
+  });
+}
+
+function savePDF() {
+  const doc = new jsPDF();
+  const imgData = fabricCanvas.toDataURL({ format: "png" });
+  doc.addImage(imgData, 0, 0, 0, 0);
+  doc.save("download.pdf");
+}
+
+function addText() {
+  const text = new fabric.IText("Hello, world!", {
+    left: 100,
+    top: 100,
+    fontSize: 30,
+  });
+  fabricCanvas.add(text);
+}
+
+function deleteActiveObject(e: KeyboardEvent) {
+  if ((e.keyCode === 46 || e.keyCode === 8) && !isEditing) {
+    const activeObject = fabricCanvas.getActiveObject();
+    if (activeObject) {
+      fabricCanvas.remove(activeObject);
+    }
+  }
+}
+
+function updateStyles(items?: any) {
+  const activeObject = fabricCanvas.getActiveObject();
+  if (activeObject) {
+    activeObject.set({
+      fontSize: items?.fontSize
+        ? parseInt(items?.fontSize)
+        : parseInt(
+            (document.getElementById("font-size") as HTMLInputElement).value
+          ),
+      fill: color,
+      fontFamily: (document.getElementById("font-family") as HTMLInputElement)
+        .value,
+      fontWeight: items?.bold ? "bold" : "normal",
+      fontStyle: items?.italic ? "italic" : "normal",
+    });
+
+    fabricCanvas.renderAll();
+    toggleEditor(true);
+  }
+}
+
+function loadImage() {
+  const imageUrl = (document.getElementById("imageUrl") as HTMLInputElement)
+    .value;
+
+  fabric.Image.fromURL(imageUrl, (img) => {
+    const oImg = img.set({ left: 0, top: 0, angle: 0 }).scale(0.5);
+    fabricCanvas.add(oImg).renderAll();
+  });
+}
+
+function handleImageUpload(e: Event) {
+  const file = (e.target as HTMLInputElement).files[0];
+  const reader = new FileReader();
+
+  reader.onload = (f) => {
+    const data: string | ArrayBuffer = f.target.result;
+    fabric.Image.fromURL(data as string, (img) => {
+      const oImg = img.set({ left: 0, top: 0, angle: 0 }).scale(0.9);
+      fabricCanvas.add(oImg).renderAll();
+      fabricCanvas.setActiveObject(oImg);
+    });
+  };
+
+  reader.readAsDataURL(file);
+}
+
+function toggleEditor(editing: boolean) {
+  const editorPanel = document.getElementById("wysiwyg-editor-container");
+  editorPanel.style.display = editing ? "block" : "none";
+}
+
+function resetStyles() {
+  color = "black";
+}
+
+function toggleUploader(showUpload: boolean) {
+  const editorPanel = document.getElementById("upload-pdf-wrapper");
+  editorPanel.style.display = showUpload ? "block" : "none";
+}
+
+function removeCurrentPdf() {
+  document.querySelector("#pdf-canvas-container > div:nth-child(1)").remove();
+  const newCanvas = document.createElement("canvas");
+  newCanvas.id = "pdf-canvas";
+  document.getElementById("pdf-canvas-container").appendChild(newCanvas);
+  toggleNavBar(false);
+}
+
+function toggleNavBar(showNavigation: boolean) {
+  const navigationBar = document.getElementById("navigation");
+  navigationBar.style.display = showNavigation ? "flex" : "none";
+}
+
+function formatBlock(size: string) {
+  switch (size) {
+    case "H1":
+      updateStyles({ fontSize: 35 });
+      break;
+    case "H2":
+      updateStyles({ fontSize: 30 });
+      break;
+    case "H3":
+      updateStyles({ fontSize: 25 });
+      break;
+    default:
+      break;
+  }
+}
